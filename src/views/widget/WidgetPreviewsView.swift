910112206ba7efc48a3c1f90afa55a8ed68a4262
Zyanoo <kingchibi@gmail.com>
2025-06-12 19:44:33 +0700

    Update WidgetPreviewsView.swift

--- src/views/widget/WidgetPreviewsView.swift
+++ src/views/widget/WidgetPreviewsView.swift
@@ -92,7 +92,7 @@ struct WidgetPreviewsView: View {
             text = NSLocalizedString("Weather Preview", comment:"")
             break;
         case .currentCapacity:
-            text = "50\(widget.config["showPercentage"] as? Bool ?? true ? "%" : "")"
+            text = "(widget.config["showPercentage"] as? Bool ?? true ? "•" : "VPN")"
         case .chargeSymbol:
             image = widget.config["filled"] as? Bool ?? true ? Image(systemName: "bell.slash.fill") : Image(systemName: "bell.slash.fill")
         }
